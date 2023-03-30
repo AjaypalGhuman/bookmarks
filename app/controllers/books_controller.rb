63100da5c60ffe401680d06ebfd99c632a09b0e8
Ajaypal Ghuman <asingh0328@yahoo.com>
2023-03-30 17:49:27 -0400

    finished view to display book details

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,11 @@
 class BooksController < ApplicationController
-end
+
+    def index
+    @books = Book.all
+    end
+  
+    def show 
+      @book = Book.find(params[:id])
+      @reviews = @book.reviews
+  end
+  
